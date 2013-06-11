8442c59ffbb3e392497c12923cc1392f8ad8d2a8
marlontaro <marloncordova@outlook.com>
2013-06-10 20:35:09 -0500

    proyecto rails

---
+++ apppc2/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Apppc2::Application.config.secret_token = '9791b5a632cf96125b3da82c9a87621d219b396abeb0f378d20a05ede9af28bb5d0dce779d82cd786ef0e2dbde9896981c50940c956336880dc853a35b2eb542'
